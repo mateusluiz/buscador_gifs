0eba7272c257e5da5c1919b6cfed3973944732dc
Mateus Luiz <mateus74890@gmail.com>
2021-11-22 18:18:36 -0300

    Buscador de Gifs 1.0

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+// @dart=2.9
+
+import 'package:flutter/material.dart';
+
+import 'package:buscador_gifs/ui/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: const HomePage(),
+    theme: ThemeData(
+      hintColor: Colors.white,
+      primaryColor: Colors.white,
+      inputDecorationTheme: const InputDecorationTheme(
+        disabledBorder:
+            OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+        enabledBorder:
+            OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+        hintStyle: TextStyle(color: Colors.white),
+      ),
+    ),
+  ));
+}
---
+++ lib/ui/home_page.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String? _search;
+  int _offset = 0;
+
+  Future<Map> _getGifs() async {
+    http.Response response;
+
+    if (_search == null) {
+      response = await http.get(
+          'https://api.giphy.com/v1/gifs/trending?api_key=dsK2mdW9g9JzcbqXPfwHHWEyj0UCSOyB&limit=20&rating=g');
+    } else {
+      response = await http.get(
+          'https://api.giphy.com/v1/gifs/search?api_key=dsK2mdW9g9JzcbqXPfwHHWEyj0UCSOyB&q=$_search&limit=20&offset=$_offset&rating=g&lang=en');
+    }
+
+    return jsonDecode(response.body);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _getGifs().then((map) {
+      print(map);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Image.network(
+          'https://developers.giphy.com/static/img/dev-logo-lg.gif',
+        ),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(10.0),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'Pequise Aqui!',
+                labelStyle: TextStyle(color: Colors.white),
+                border: OutlineInputBorder(),
+              ),
+              style: TextStyle(color: Colors.white, fontSize: 18.0),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Expanded(
+            child: FutureBuilder(
+              future: _getGifs(),
+              builder: (context, snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.waiting:
+                  case ConnectionState.none:
+                    return Container(
+                      width: 200.0,
+                      height: 200.0,
+                      alignment: Alignment.center,
+                      child: const CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                        strokeWidth: 5.0,
+                      ),
+                    );
+                  default:
+                    if (snapshot.hasError) {
+                      return Container();
+                    } else {
+                      return _createGifTable(context, snapshot);
+                    }
+                }
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, crossAxisSpacing: 10.0, mainAxisSpacing: 10.0),
+      itemCount: snapshot.data["data"].length,
+      itemBuilder: (context, index) {
+        return GestureDetector(
+          child: Image.network(
+            snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+            height: 300.0,
+            fit: BoxFit.cover,
+          ),
+        );
+      },
+    );
+  }
+}
